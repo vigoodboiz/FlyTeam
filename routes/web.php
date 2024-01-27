d965cd79314cf818accc53b7cbbc61b79c7de191
lathehiep <hunghoixu@gmail.com>
2024-01-27 14:12:24 +0700

    moi

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+
+///////////////////////// product //////////////////
 Route::get('/products', [ProductController::class, 'index'])->name('products.index');
 // Route::get('/products/create', [ProductController::class, 'create']);
 Route::get('/products/create', [ProductController::class, 'create'])->name('products.create');
