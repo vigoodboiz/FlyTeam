097b53c4c646cee42137ab6a0696a625e3ef4e44
duy niên <115699771+nien1321@users.noreply.github.com>
2024-02-03 15:53:21 +0700

    update

--- resources/views/layouts/components/sidebar.blade.php
+++ resources/views/layouts/components/sidebar.blade.php
@@ -7,75 +7,36 @@
                 </li>
                 <li class="menu-title">Admin</li><!-- /.menu-title -->
                 <li class="menu-item-has-children dropdown">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true"
-                        aria-expanded="false"> <i class="menu-icon fa fa-cogs"></i>Danh sách quản lý</a>
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> <i class="menu-icon fa fa-cogs"></i>Danh sách quản lý</a>
                     <ul class="sub-menu children dropdown-menu">
-                        {{-- @can('user_access') --}}
-                        <li><i class="fa fa-puzzle-piece"></i><a href="">Quản lý người
-                                dùng</a></li>
-                        {{-- @endcan --}}
-                        {{-- <li><i class="fa fa-id-badge"></i><a href="">Quản lý khách hàng</a>
-                        </li> --}}
-                        <li><i class="fa fa-bars"></i><a href="">Quản lý phân quyền</a>
-                        </li>
+                         @can('user_access') 
+                        <li><i class="fa fa-puzzle-piece"></i><a href="">Quản lý người dùng</a></li>
+                         @endcan 
+
+                        <li><i class="fa fa-bars"></i><a href="">Quản lý phân quyền</a></li>
                         {{-- @can('role_access') --}}
                         <li><i class="fa fa-id-card-o"></i><a href="">Quản lý vai trò</a></li>
                         {{-- @endcan --}}
-                        <li><i class="fa fa-exclamation-triangle"></i><a href="ui-alerts.html">Quản lý sản phẩm</a></li>
-                        <li><i class="fa fa-spinner"></i><a href="ui-progressbar.html">Quản lý danh mục</a>
-                        <li><i class="bi bi-person-circle text-Danger"></i><a href="{{ route('users.index') }}">Quản lý
-                                người dùng</a></li>
-
-                        <li><i class="bi bi-people text-Danger"></i><a href="{{ route('members.index') }}">Quản lý khách
-                                hàng</a>
-                        </li>
-                        <li><i class="bi bi-person-bounding-box"></i></i><a href="ui-tabs.html">Quản lý phân quyền</a>
-                        </li>
-
-                        <li><i class="bi bi-person-vcard-fill text-Danger"></i><a href="ui-cards.html">Quản lý vai
-                                trò</a></li>
-
-                        <li><i class="bi bi-inboxes-fill text-Danger"></i><a
-                                href="{{ route('admin.categories.index') }}">Quản lý sản phẩm</a></li>
-                        <li><i class="bi bi-grid-3x3-gap text-Danger"></i></i><a
-                                href="{{ route('products.index') }}">Quản lý danh mục</a>
-
-                        <li><i class="bi bi-inboxes-fill text-Danger"></i><a href="ui-alerts.html">Quản lý sản phẩm</a>
-                        </li>
-                        <li><i class="bi bi-grid-3x3-gap text-Danger"></i></i><a href="ui-progressbar.html">Quản lý danh
-                                mục</a>
-
+                        <li><i class="bi bi-people text-Danger"></i><a href="{{ route('members.index') }}">Quản lý khách hàng</a></li>
                         <li><i class="bi bi-inboxes-fill text-Danger"></i><a href="{{route('products.index')}}">Quản lý sản phẩm</a></li>
-                        <li><i class="bi bi-grid-3x3-gap text-Danger"></i></i><a href="{{route('categories.index')}}">Quản lý danh mục</a>
-
-                        </li>
-
-                        <li><i class="bi bi-bag-fill text-Danger"></i><a href="{{ route('listOder') }}">Quản lý đơn
-                                hàng</a></li>
-                        <li><i class="bi bi-chat-quote text-Danger"></i><a
-                                href="{{ route('route_comment_index') }}">Quản lý bình luận</a></li>
-                        <li><i class="bi bi-gift-fill text-Danger"></i></i><a href="ui-grids.html">Quản lý khuyến
-                                mại</a></li>
-                        <li><i class="bi bi-minecart text-Danger"></i><a href="{{ route('listOder_status') }}">Trạng
-                                thái đơn hàng</a></li>
-                        <li><i class="bi bi-truck text-Danger"></i><a href="{{ route('listDelivery_status') }}">Trạng
-                                thái giao hàng</a></li>
-                        <li><i class="bi bi-graph-up-arrow text-Danger"></i><a href="ui-typgraphy.html">Thống kê</a>
-                        </li>
-
+                        <li><i class="bi bi-grid-3x3-gap text-Danger"></i></i><a href="{{route('categories.index')}}">Quản lý danh mục</a></li>
+                        <li><i class="bi bi-bag-fill text-Danger"></i><a href="{{ route('listOder') }}">Quản lý đơn hàng</a></li>
+                        <li><i class="bi bi-chat-quote text-Danger"></i><a href="{{ route('route_comment_index') }}">Quản lý bình luận</a></li>
+                        <li><i class="bi bi-gift-fill text-Danger"></i></i><a href="ui-grids.html">Quản lý khuyến mại</a></li>
+                        <li><i class="bi bi-minecart text-Danger"></i><a href="{{ route('listOder_status') }}">Trạng thái đơn hàng</a></li>
+                        <li><i class="bi bi-truck text-Danger"></i><a href="{{ route('listDelivery_status') }}">Trạng thái giao hàng</a></li>
+                        <li><i class="bi bi-graph-up-arrow text-Danger"></i><a href="ui-typgraphy.html">Thống kê</a></li>
                     </ul>
                 </li>
                 <li class="menu-item-has-children dropdown">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true"
-                        aria-expanded="false"> <i class="menu-icon fa fa-table"></i>Tables</a>
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> <i class="menu-icon fa fa-table"></i>Tables</a>
                     <ul class="sub-menu children dropdown-menu">
                         <li><i class="fa fa-table"></i><a href="tables-basic.html">Basic Table</a></li>
                         <li><i class="fa fa-table"></i><a href="tables-data.html">Data Table</a></li>
                     </ul>
                 </li>
                 <li class="menu-item-has-children dropdown">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true"
-                        aria-expanded="false"> <i class="menu-icon fa fa-th"></i>Forms</a>
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> <i class="menu-icon fa fa-th"></i>Forms</a>
                     <ul class="sub-menu children dropdown-menu">
                         <li><i class="menu-icon fa fa-th"></i><a href="forms-basic.html">Basic Form</a></li>
                         <li><i class="menu-icon fa fa-th"></i><a href="forms-advanced.html">Advanced Form</a></li>
@@ -85,8 +46,7 @@
                 <li class="menu-title">Icons</li><!-- /.menu-title -->
 
                 <li class="menu-item-has-children dropdown">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true"
-                        aria-expanded="false"> <i class="menu-icon fa fa-tasks"></i>Icons</a>
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> <i class="menu-icon fa fa-tasks"></i>Icons</a>
                     <ul class="sub-menu children dropdown-menu">
                         <li><i class="menu-icon fa fa-fort-awesome"></i><a href="font-fontawesome.html">Font Awesome</a>
                         </li>
@@ -97,8 +57,7 @@
                     <a href="widgets.html"> <i class="menu-icon ti-email"></i>Widgets </a>
                 </li>
                 <li class="menu-item-has-children dropdown">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true"
-                        aria-expanded="false"> <i class="menu-icon fa fa-bar-chart"></i>Charts</a>
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> <i class="menu-icon fa fa-bar-chart"></i>Charts</a>
                     <ul class="sub-menu children dropdown-menu">
                         <li><i class="menu-icon fa fa-line-chart"></i><a href="charts-chartjs.html">Chart JS</a></li>
                         <li><i class="menu-icon fa fa-area-chart"></i><a href="charts-flot.html">Flot Chart</a></li>
@@ -107,8 +66,7 @@
                 </li>
 
                 <li class="menu-item-has-children dropdown">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true"
-                        aria-expanded="false"> <i class="menu-icon fa fa-area-chart"></i>Maps</a>
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> <i class="menu-icon fa fa-area-chart"></i>Maps</a>
                     <ul class="sub-menu children dropdown-menu">
                         <li><i class="menu-icon fa fa-map-o"></i><a href="maps-gmap.html">Google Maps</a></li>
                         <li><i class="menu-icon fa fa-street-view"></i><a href="maps-vector.html">Vector Maps</a></li>
@@ -116,8 +74,7 @@
                 </li>
                 <li class="menu-title">Extras</li><!-- /.menu-title -->
                 <li class="menu-item-has-children dropdown">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true"
-                        aria-expanded="false"> <i class="menu-icon fa fa-glass"></i>Pages</a>
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> <i class="menu-icon fa fa-glass"></i>Pages</a>
                     <ul class="sub-menu children dropdown-menu">
                         <li><i class="menu-icon fa fa-sign-in"></i><a href="page-login.html">Login</a></li>
                         <li><i class="menu-icon fa fa-sign-in"></i><a href="page-register.html">Register</a></li>
--- routes/web.php
+++ routes/web.php
@@ -113,40 +113,36 @@ Route::prefix('admin')->middleware(['auth'])->group(function () {
     Route::match(['GET', 'POST'], '/comment/delete/{id}', [CommentController::class, 'delete'])->name('route_comment_delete');
 
 
-     ///////////////////////// product //////////////////
-     Route::get('/products', [ProductController::class, 'index'])->name('products.index');
-     // Route::get('/products/create', [ProductController::class, 'create']);
-     Route::get('/products/create', [ProductController::class, 'create'])->name('products.create');
-     Route::post('/products', [ProductController::class, 'store'])->name('products.store');
+    ///////////////////////// product //////////////////
+    Route::get('/products', [ProductController::class, 'index'])->name('products.index');
+    // Route::get('/products/create', [ProductController::class, 'create']);
+    Route::get('/products/create', [ProductController::class, 'create'])->name('products.create');
+    Route::post('/products', [ProductController::class, 'store'])->name('products.store');
 
-     Route::get('/products/{id}/edit', [ProductController::class, 'edit'])->name('products.edit');
-     Route::put('/products/{id}', [ProductController::class, 'update'])->name('products.update');
-
-
-     Route::delete('/products/{id}', [ProductController::class, 'destroy'])->name('product.destroy');
+    Route::get('/products/{id}/edit', [ProductController::class, 'edit'])->name('products.edit');
+    Route::put('/products/{id}', [ProductController::class, 'update'])->name('products.update');
 
 
-     ///////////////////////// cate //////////////////
+    Route::delete('/products/{id}', [ProductController::class, 'destroy'])->name('product.destroy');
 
 
-     Route::get('/categories', [CategoryController::class, 'index'])->name('admin.categories.index');
+    ///////////////////////// cate //////////////////
 
+
+    
+    Route::get('/categories', [CategoryController::class, 'index'])->name('categories.index');
+
+
+    Route::get('/categories/create', [CategoryController::class, 'create'])->name('categories.create');
+    Route::post('/categories', [CategoryController::class, 'store'])->name('categories.store');
+
+    Route::get('/categories/{category}/edit', [CategoryController::class, 'edit'])->name('categories.edit');
+    Route::put('/categories/{category}', [CategoryController::class, 'update'])->name('categories.update');
+
+    Route::delete('/categories/{category}', [CategoryController::class, 'destroy'])->name('categories.destroy');
 });
 
-     Route::get('/categories', [CategoryController::class, 'index'])->name('categories.index');
-     
 
-     Route::get('/categories/create', [CategoryController::class, 'create'])->name('categories.create');
-     Route::post('/categories', [CategoryController::class, 'store'])->name('categories.store');
-
-     Route::get('/categories/{category}/edit', [CategoryController::class, 'edit'])->name('categories.edit');
-     Route::put('/categories/{category}', [CategoryController::class, 'update'])->name('categories.update');
-
-     Route::delete('/categories/{category}', [CategoryController::class, 'destroy'])->name('categories.destroy');
-
-
-
-});
 
 //Comments
 Route::get('/getComments', [CommentController::class, 'index'])->name('route_comment_index');
@@ -159,8 +155,8 @@ Route::get('/dashboard', [App\Http\Controllers\DashboardController::class, 'inde
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
 })->name('dashboard');
-  //facebook
-Route::controller(FacebookController::class)->group(function(){
+//facebook
+Route::controller(FacebookController::class)->group(function () {
     Route::get('auth/facebook', 'redirectToFacebook')->name('auth.facebook');
     Route::get('auth/facebook/callback', 'handleFacebookCallback');
 });
